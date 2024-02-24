4acdbaa802e8d1119a2bd781f026d42a865a3548
Bill Tihen <btihen@gmail.com>
2024-02-24 14:36:03 +0100

    add users to the 'core'

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ import Config
 
 config :authorize,
   ecto_repos: [Authorize.Repo],
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime, binary_id: true]
 
 # Configures the endpoint
 config :authorize, AuthorizeWeb.Endpoint,
